61e4fe659a684743b1e0e4bd56302db344f44426
armink <armink.ztl@gmail.com>
2021-01-08 16:19:06 +0800

    【修正】windows msvc 编译错误。
    Signed-off-by: armink <armink.ztl@gmail.com>

--- packages/FlashDB/src/fdb.c
+++ packages/FlashDB/src/fdb.c
@@ -102,8 +102,10 @@ void _fdb_deinit(fdb_db_t db)
 
     if (db->init_ok) {
 #ifdef FDB_USING_FILE_POSIX_MODE
-#include <unistd.h>
         if (db->cur_file > 0) {
+#if !defined(_MSC_VER)
+#include <unistd.h>
+#endif
             close(db->cur_file);
         }
 #else
